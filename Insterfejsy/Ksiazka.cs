31545a6c94c47e0a0c670967f02caa8f317f6f86
Pilarski <139373074+Pikaki02@users.noreply.github.com>
2023-12-07 10:45:16 +0100

    Add files via upload

---
+++ Insterfejsy/Ksiazka.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insterfejsy
+{
+    internal class Ksiazka : IComparable<Ksiazka>
+    {
+        public string Tytul {  get; set; }
+        public string Autor { get; set; }
+        public int RokWydania { get; set; }
+        public double Cena { get; set; }
+
+        public Ksiazka(string tytul,string autor,int rokWydania,double cena ) 
+        {
+            Tytul = tytul;
+            Autor = autor;
+            RokWydania = rokWydania;
+            Cena = cena;
+        }
+        public override string ToString()
+        {
+            return $"{Tytul}, {Autor}, {RokWydania}, {Cena} zł";
+        }
+        public int CompareTo([AllowNull] Ksiazka other)
+        {
+
+            if (other == null)
+            {
+                return 1;
+            }
+            return Cena.CompareTo(other.Cena);
+        }
+        
+    }
+}
---
+++ Insterfejsy/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Insterfejsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ksiazka> ks = new List<Ksiazka>();
+            ks.Add(new Ksiazka("Hobbit", "Tolkien2", 1936, 20));
+            ks.Add(new Ksiazka("Hobbit2", "Tolkien1", 1937, 30));
+            ks.Add(new Ksiazka("Hobbit3", "Tolkien3", 1938, 10));
+            ks.Sort();
+            foreach (Ksiazka ks2 in ks)
+            {
+                Console.WriteLine(ks2);
+            }
+            Console.WriteLine();
+            var sortedByYear = ks.OrderBy(b => b.RokWydania);
+            foreach (Ksiazka ks2 in sortedByYear)
+            {
+                Console.WriteLine(ks2);
+            }
+            Console.WriteLine();
+            var sortedByAuthor = ks.OrderByDescending(b => b.Autor);
+            foreach (Ksiazka ks2 in sortedByAuthor)
+            {
+                Console.WriteLine(ks2);
+            }
+            Console.WriteLine();
+            var sortedByPriceAndYear = ks.OrderByDescending(b => b.Cena).ThenBy(b => b.RokWydania);
+            foreach (Ksiazka ks2 in sortedByPriceAndYear)
+            {
+                Console.WriteLine(ks2);
+            }
+        }
+    }
+}
