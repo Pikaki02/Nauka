ceb46eca14739b809d42836a59dfb5e6e531a921
Pilarski <139373074+Pikaki02@users.noreply.github.com>
2023-12-07 11:16:00 +0100

    Add files via upload

---
+++ Delegaty_Zdarzenia_Interfejsy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegaty_Zdarzenia_Interfejsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Delegaty_Zdarzenia_Interfejsy/RandomList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegaty_Zdarzenia_Interfejsy
+{
+    internal class RandomList : IRandomList
+    {
+        private List<int> list;
+        public int Count 
+        { 
+            get 
+            { 
+                return list.Count; 
+            } 
+        }
+        public RandomList(int length, int min, int max)
+        {
+            list = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(rnd.Next(min, max+1));
+            }
+
+            if(list.Count == 0)
+            {
+                EmptyList?.Invoke();
+            }
+        }
+    }
+}
